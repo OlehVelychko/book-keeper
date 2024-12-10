ec12dcb77e82f13e99faf934d2a180712772b397
OlehVelychko <70134299+OlehVelychko@users.noreply.github.com>
2024-12-10 19:13:48 +0200

    Initial commit

---
+++ src/main/java/dao/PersonDAO.java
@@ -0,0 +1,48 @@
+package dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+import models.Person;
+
+import java.util.List;
+
+@Component
+public class PersonDAO {
+
+    private static final String SELECT_ALL = "SELECT * FROM book_keeper_schema.person";
+    private static final String SELECT_BY_ID = "SELECT * FROM book_keeper_schema.person WHERE id=?";
+    private static final String INSERT_PERSON = "INSERT INTO book_keeper_schema.person VALUES(1, ?, ?, ?)";
+    private static final String UPDATE_PERSON = "UPDATE book_keeper_schema.person SET name=?, age=?, email=? WHERE id=?";
+    private static final String DELETE_PERSON = "DELETE FROM book_keeper_schema.person WHERE id=?";
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public PersonDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<Person> index() {
+        return jdbcTemplate.query(SELECT_ALL, new PersonMapper());
+    }
+
+    public Person show(int id) {
+        return jdbcTemplate.query(SELECT_BY_ID, new PersonMapper(), new Object[]{id})
+                .stream().findAny().orElse(null);
+    }
+
+    public void save(Person person) {
+        jdbcTemplate.update(INSERT_PERSON, person.getName(), person.getAge(),
+                person.getEmail());
+    }
+
+    public void update(int id, Person updatedPerson) {
+        jdbcTemplate.update(UPDATE_PERSON, updatedPerson.getName(),
+                updatedPerson.getAge(), updatedPerson.getEmail(), id);
+    }
+
+    public void delete(int id) {
+        jdbcTemplate.update(DELETE_PERSON, id);
+    }
+}
